f4ab865d8b517b0431537ebe229f1189b77f201a
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-13 19:40:49 +0100

    Maitrisez-la-creation-de-jeux-web-JavaScript-avec-Phaser-v3-02-Phaser3LesBases-E5-SpriteEventOnClick

--- Programmes/2-Phaser3LesBases/BaseServeur/js_client/main.js
+++ Programmes/2-Phaser3LesBases/BaseServeur/js_client/main.js
@@ -10,23 +10,52 @@ var config = {
 }
 
 var player = null;
+var clickBoutonHaut = false;
+var clickBoutonBas = false;
 
 const game = new Phaser.Game(config);
 
 function preload() {
-    console.log(this);
     this.load.image("joueur","player.png");
+    this.load.image("haut","haut.png");
+    this.load.image("bas","bas.png");
 }
 
 function create() {
     var positionCameraCentreX = this.cameras.main.centerX;
     var positionCameraCentreY = this.cameras.main.centerY;
     player = this.add.sprite(positionCameraCentreX,positionCameraCentreY,"joueur");
-    player.setScale(1.5);
-    player.setOrigin(0,0);
-    player.setFlip(true,true);
+
+    var down = this.add.sprite(50,50,"bas").setInteractive();
+    var top = this.add.sprite(100,50,"haut").setInteractive();
+
+    down.on("pointerdown", function() {
+        clickBoutonBas = true;
+    });
+    down.on("pointerup", function() {
+        clickBoutonBas = false;
+    })
+    down.on("pointerout", function() {
+        clickBoutonBas = false;
+    })
+
+    top.on("pointerdown", function() {
+        clickBoutonHaut = true;
+    });
+    top.on("pointerup", function() {
+        clickBoutonHaut = false;
+    })
+    top.on("pointerout", function() {
+        clickBoutonHaut = false;
+    })
 }
 
 function update(time, delta) {
     // player.setAngle(player.angle + 1);
+    if(clickBoutonHaut) {
+        player.setScale(player.scaleX + 0.1, player.scaleY + 0.1);
+    }
+    if(clickBoutonBas) {
+        player.setScale(player.scaleX - 0.1, player.scaleY - 0.1);
+    }
 }
