b94df12f99941bfb1775c1f575b5ee666bd35ba5
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-13 17:46:15 +0100

    Maitrisez-la-creation-de-jeux-web-JavaScript-avec-Phaser-v3-01-RealisationDuServeurAvecNode.JS-E6-FinalisationDuServeur

--- Programmes/1-RealisationDuServeurAvecNode.JS/BaseServeur/serveur.js
+++ Programmes/1-RealisationDuServeurAvecNode.JS/BaseServeur/serveur.js
@@ -2,56 +2,25 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 
+var gestionPage = require("./gestionPage");
+
 const PORT = "8080";
 
 var serveur = http.createServer(traitReq);
 serveur.listen(PORT);
 
-function traitReq(requete, reponse) {
+function traitReq(requete,reponse) {
     var monObj = url.parse(requete.url);
-    var contentType = "";
 
     if(monObj.pathname === "/") {
         monObj.pathname = "/index.html";
     }
-
-    var encodage = "";
-    var dossier = "";
-
-    var indexDuPoint = monObj.pathname.indexOf(".");
-    var extension = monObj.pathname.substring(indexDuPoint,monObj.pathname.length);
-    var fichier = monObj.pathname.substring(1,monObj.pathname.length);
-
-    switch(extension) {
-        case ".html" : 
-            contentType = "text/html";
-            encodage = "UTF-8";
-            dossier = "html/"
-        break;
-        case ".css" : 
-            contentType = "text/css";
-            dossier = "css/"
-        break;
-        case ".js" : 
-            contentType = "application/javascript";
-            dossier = "js_client/"
-        break;
-        case ".png" : 
-            contentType = "image/png";
-            dossier = "assets/"
-        break;
-        case ".jpf" : 
-            contentType = "image/jpeg";
-            dossier = "assets/"
-        break;
-        default : console.log("Erreur type d'extension non reconnu");
-    }
     
     if(monObj.pathname !== "/favicon.ico") {
-        var pageHtml = fs.readFileSync(dossier + fichier,encodage);
-
-        reponse.writeHead(200,{"Content-Type" : contentType});
-        reponse.write(pageHtml);
-        reponse.end();
+        var dataPreparation = gestionPage.preparerLesDonnees(monObj);
+        var data = {};
+        data.contentType = dataPreparation.contentType;
+        data.pageHtml = fs.readFileSync(dataPreparation.dossier + dataPreparation.fichier, dataPreparation.encodage);
+        gestionPage.envoyerLesDonnees(reponse,data);
     }
 }
