cea5af62a4849a52a75fa9d949dbfa84029d5cf6
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-14 19:30:38 +0100

    Maitrisez-la-creation-de-jeux-web-JavaScript-avec-Phaser-v3-03-JeuDePlateforme-E12-Overlap(recupererDesObjets)

--- Programmes/3-JeuDePlateforme/3JeuPlateforme/js_client/objets/world.js
+++ Programmes/3-JeuDePlateforme/3JeuPlateforme/js_client/objets/world.js
@@ -4,6 +4,7 @@ var world = {
     downLayer : null,
     worldLayer : null,
     topLayer : null,
+    overlapLayer : null,
 
     initialiserWorld : function() {
         this.tilemap = jeu.scene.make.tilemap({key: "map"});
@@ -11,19 +12,26 @@ var world = {
         this.downLayer = this.tilemap.createStaticLayer("bot",this.tileset,0,0);
         this.worldLayer = this.tilemap.createStaticLayer("world",this.tileset,0,0);
         this.topLayer = this.tilemap.createStaticLayer("top",this.tileset,0,0);
+        this.overlapLayer = this.tilemap.createDynamicLayer("overlap",this.tileset,0,0);
 
         this.worldLayer.setCollisionByProperty({Collides : true});
-        
+
         jeu.scene.physics.world.setBounds(0,0,this.tilemap.widthInPixels,this.tilemap.heightInPixels);
     },
 
     gererCollider : function() {
-        jeu.scene.physics.add.collider(jeu.player.aPlayer,this.worldLayer);
+        this.overlapLayer.setTileIndexCallback(50,this.collectGemme,this); 
+        this.overlapLayer.setTileIndexCallback(53,this.collectGemme,this);
+        jeu.scene.physics.add.collider(jeu.player.aPlayer,this.worldLayer)
+        jeu.scene.physics.add.overlap(jeu.player.aPlayer,this.overlapLayer);
     },
     
     gererCamera : function() {
         jeu.scene.cameras.main.startFollow(jeu.player.aPlayer);
         jeu.scene.cameras.main.setBounds(0,0,this.tilemap.widthInPixels,this.tilemap.heightInPixels);
+    },
+    
+    collectGemme : function (player,tile) {
+        this.overlapLayer.removeTileAt(tile.x,tile.y).destroy();
     }
-   
 }
