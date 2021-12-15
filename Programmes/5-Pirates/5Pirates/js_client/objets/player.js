1c01670229d13b5b267adafafadd7a9a1d67fbb4
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-15 21:54:41 +0100

    Maitrisez-la-creation-de-jeux-web-JavaScript-avec-Phaser-v3-05-Pirates-E3-AjoutDuPlayer

--- Programmes/5-Pirates/5Pirates/js_client/objets/player.js
+++ Programmes/5-Pirates/5Pirates/js_client/objets/player.js
@@ -1,15 +1,18 @@
 var player = {
     aPlayer : null,
 
-    initialiserPlayer : function(){
-
+    initialiserPlayer : function() {
+        this.aPlayer = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x,jeu.world.positionDebut.y,"player");
+        this.aPlayer.setSize(50,50);
+        this.aPlayer.setFlip(false,true);
+        this.aPlayer.setCollideWorldBounds(true);
     },
     
-    generatePlayerAnimations : function(){
+    generatePlayerAnimations : function() {
        
     },
 
-    gererDeplacement : function(){
+    gererDeplacement : function() {
       
     }
 }
