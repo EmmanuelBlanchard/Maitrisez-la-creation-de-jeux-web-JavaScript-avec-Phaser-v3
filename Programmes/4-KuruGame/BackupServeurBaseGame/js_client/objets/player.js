6354ad63dd8ce605a2b82ded6c24d1c821d66ee9
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-15 19:10:25 +0100

    Maitrisez-la-creation-de-jeux-web-JavaScript-avec-Phaser-v3-04-KuruGame-E10-ChangementDeLevel

--- Programmes/4-KuruGame/BackupServeurBaseGame/js_client/objets/player.js
+++ Programmes/4-KuruGame/BackupServeurBaseGame/js_client/objets/player.js
@@ -10,8 +10,8 @@ var player = {
         this.playerBarre = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x,jeu.world.positionDebut.y,"playerBarre").setScale(2);
         this.playerCenter = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x,jeu.world.positionDebut.y,"playerCenter").setScale(2);
 
-        this.ident = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x + this.playerBarre.width,jeu.world.positionDebut.y,"playerIdent").setScale(0.5);
-        this.ident2 = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x - this.playerBarre.width,jeu.world.positionDebut.y,"playerIdent").setScale(0.5);
+        this.ident = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x + this.playerBarre.width,jeu.world.positionDebut.y).setScale(0.5);
+        this.ident2 = jeu.scene.physics.add.sprite(jeu.world.positionDebut.x - this.playerBarre.width,jeu.world.positionDebut.y).setScale(0.5);
     },
     
     generatePlayerAnimations : function() {
@@ -19,15 +19,18 @@ var player = {
     },
 
     gererDeplacement : function() {
-        if(this.ableToMove){
+        if(this.ableToMove) {
             if(jeu.cursor.left.isDown) this.playerCenter.x -= this.speed ;
             if(jeu.cursor.right.isDown) this.playerCenter.x += this.speed;
             if(jeu.cursor.up.isDown) this.playerCenter.y -= this.speed;
             if(jeu.cursor.down.isDown) this.playerCenter.y += this.speed;
+            if(jeu.cursor.space.isDown) this.speed = 10;
+            if(jeu.cursor.space.isUp) this.speed = 5;
         }
         this.playerBarre.x = this.playerCenter.x;
         this.playerBarre.y = this.playerCenter.y;
     },
+
     gererRotation() {
         var angle = this.playerBarre.angle - 1.5;
         this.playerBarre.setAngle(angle);
--- Programmes/4-KuruGame/BackupServeurBaseGame/js_client/objets/world.js
+++ Programmes/4-KuruGame/BackupServeurBaseGame/js_client/objets/world.js
@@ -16,7 +16,7 @@ var world = {
     isLevelFin : false,
 
     initialiserWorld : function() {
-        this.tilemap = jeu.scene.make.tilemap({key: "map"});
+        this.tilemap = jeu.scene.make.tilemap({key: "map"+jeu.level});
         this.tilesetTerrain = this.tilemap.addTilesetImage("terrain","terrain");
         this.tilesetItem = this.tilemap.addTilesetImage("tilesPerso","tilesPerso");
         this.downLayerTerrain = this.tilemap.createStaticLayer("botTerrain",this.tilesetTerrain,0,0);
@@ -43,21 +43,16 @@ var world = {
         this.scoreText = jeu.scene.add.text (16,16,"Score : 0",policeTitre);
         this.scoreText.setScrollFactor(0);
     },
-    gererCollider : function() {
-        jeu.scene.physics.add.overlap(jeu.player.playerCenter,this.drapeauFin, this.finLevel);
-        this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + 21,this.contactPlayerWorld,this);
-        this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + 22,this.contactPlayerWorld,this);
-        this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + 33,this.contactPlayerWorld,this);
-        this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + 34,this.contactPlayerWorld,this);
-        this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + 35,this.contactPlayerWorld,this);
-        this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + 36,this.contactPlayerWorld,this);
+    gererCollider : function(){
+        jeu.scene.physics.add.overlap(jeu.player.playerCenter,this.drapeauFin,this.finLevel);
+       
+        this.genererColliderWorld();
+        this.genererColliderItem();
 
-        this.worldLayerItem.setTileIndexCallback(this.tilesetItem.firstgid + 6,this.contactPlayerWorld,this);
-
-        jeu.scene.physics.add.overlap(jeu.player.ident, this.worldLayer);
-        jeu.scene.physics.add.overlap(jeu.player.ident2, this.worldLayer);
-        jeu.scene.physics.add.overlap(jeu.player.ident, this.worldLayerItem);
-        jeu.scene.physics.add.overlap(jeu.player.ident2, this.worldLayerItem);
+        jeu.scene.physics.add.overlap(jeu.player.ident,this.worldLayer);
+        jeu.scene.physics.add.overlap(jeu.player.ident2,this.worldLayer);
+        jeu.scene.physics.add.overlap(jeu.player.ident,this.worldLayerItem);
+        jeu.scene.physics.add.overlap(jeu.player.ident2,this.worldLayerItem);
     },
     gererCamera : function() {
         jeu.scene.cameras.main.startFollow(jeu.player.playerCenter);
@@ -65,15 +60,34 @@ var world = {
     },
     finLevel : function() {
         jeu.player.ableToMove = false;
-        if(!jeu.world.isLevelFin) {
+        if(!jeu.world.isLevelFin){
             jeu.scene.physics.moveTo(jeu.player.playerCenter,jeu.world.positionFin.x,jeu.world.positionFin.y,100);
         }
-        if(jeu.player.playerCenter.x > jeu.world.positionFin.x -2 && jeu.player.playerCenter.x < jeu.world.positionFin.x + 2) {
+        if(jeu.player.playerCenter.x > jeu.world.positionFin.x -2 && jeu.player.playerCenter.x < jeu.world.positionFin.x + 2){
             jeu.player.playerCenter.body.stop();
             jeu.world.isLevelFin = true;
+            jeu.world.nextLevel(); 
         }
     },
     contactPlayerWorld : function() {
-        console.log("coucou");
+        jeu.scene.scene.restart();
+    },
+    genererColliderItem : function() {
+        for(var i = 0 ; i <= 17; i++) {
+            this.worldLayerItem.setTileIndexCallback(this.tilesetItem.firstgid + i,this.contactPlayerWorld,this);
+        }
+    },
+    genererColliderWorld : function() {
+        for(var i = 21 ; i <= 39; i++) {
+            if(i !== 30) {
+                this.worldLayer.setTileIndexCallback(this.tilesetTerrain.firstgid + i,this.contactPlayerWorld,this);
+            }
+        }
+    },
+    nextLevel : function() {
+        jeu.level++;
+        jeu.scene.scene.restart();
+        jeu.player.ableToMove = true;
+        this.isLevelFin = false;
     }
 }
