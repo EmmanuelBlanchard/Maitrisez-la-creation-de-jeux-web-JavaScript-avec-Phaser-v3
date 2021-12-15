e7610c3a6b0d41950e649b2d818e32e378fa18ab
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-15 19:37:59 +0100

    Maitrisez-la-creation-de-jeux-web-JavaScript-avec-Phaser-v3-04-KuruGame-E11-AjoutDePieces(TiledObjects)

--- Programmes/4-KuruGame/BackupServeurBaseGame/js_client/objets/world.js
+++ Programmes/4-KuruGame/BackupServeurBaseGame/js_client/objets/world.js
@@ -14,6 +14,7 @@ var world = {
     score : 0,
     scoreText : null,
     isLevelFin : false,
+    nbPieces : null,
 
     initialiserWorld : function() {
         this.tilemap = jeu.scene.make.tilemap({key: "map"+jeu.level});
@@ -27,6 +28,7 @@ var world = {
         this.overlapLayer = this.tilemap.createDynamicLayer("overlap",this.tilesetTerrain,0,0);
 
         this.positionDebut = this.tilemap.findObject("Objects", obj => obj.name === "Debut");
+        this.nbPieces = this.positionDebut.properties[0].value;
         this.positionFin = this.tilemap.findObject("Objects", obj => obj.name === "Fin");
         jeu.scene.add.sprite(this.positionDebut.x, this.positionDebut.y,"debut").setScale(3);
         this.drapeauFin = jeu.scene.physics.add.sprite(this.positionFin.x, this.positionFin.y,"fin").setScale(3);
@@ -44,15 +46,16 @@ var world = {
         this.scoreText.setScrollFactor(0);
     },
     gererCollider : function(){
-        jeu.scene.physics.add.overlap(jeu.player.playerCenter,this.drapeauFin,this.finLevel);
+        jeu.scene.physics.add.overlap(jeu.player.playerCenter,this.drapeauFin, this.finLevel);
        
+        this.genererPieces();
         this.genererColliderWorld();
         this.genererColliderItem();
 
-        jeu.scene.physics.add.overlap(jeu.player.ident,this.worldLayer);
-        jeu.scene.physics.add.overlap(jeu.player.ident2,this.worldLayer);
-        jeu.scene.physics.add.overlap(jeu.player.ident,this.worldLayerItem);
-        jeu.scene.physics.add.overlap(jeu.player.ident2,this.worldLayerItem);
+        jeu.scene.physics.add.overlap(jeu.player.ident, this.worldLayer);
+        jeu.scene.physics.add.overlap(jeu.player.ident2, this.worldLayer);
+        jeu.scene.physics.add.overlap(jeu.player.ident, this.worldLayerItem);
+        jeu.scene.physics.add.overlap(jeu.player.ident2, this.worldLayerItem);
     },
     gererCamera : function() {
         jeu.scene.cameras.main.startFollow(jeu.player.playerCenter);
@@ -60,16 +63,18 @@ var world = {
     },
     finLevel : function() {
         jeu.player.ableToMove = false;
-        if(!jeu.world.isLevelFin){
+        if(!jeu.world.isLevelFin) {
             jeu.scene.physics.moveTo(jeu.player.playerCenter,jeu.world.positionFin.x,jeu.world.positionFin.y,100);
         }
-        if(jeu.player.playerCenter.x > jeu.world.positionFin.x -2 && jeu.player.playerCenter.x < jeu.world.positionFin.x + 2){
+        if(jeu.player.playerCenter.x > jeu.world.positionFin.x -2 && jeu.player.playerCenter.x < jeu.world.positionFin.x + 2) {
+            jeu.scene.sound.play("win");
             jeu.player.playerCenter.body.stop();
             jeu.world.isLevelFin = true;
             jeu.world.nextLevel(); 
         }
     },
     contactPlayerWorld : function() {
+        jeu.scene.sound.play("hurt");
         jeu.scene.scene.restart();
     },
     genererColliderItem : function() {
@@ -89,5 +94,27 @@ var world = {
         jeu.scene.scene.restart();
         jeu.player.ableToMove = true;
         this.isLevelFin = false;
+    },
+    genererPieces : function() {
+        for (var i = 1 ; i <= this.nbPieces ; i++) {
+            var object = this.tilemap.findObject("Objects", obj => obj.name === "Piece"+i);
+            var piece = jeu.scene.physics.add.sprite(object.x,object.y,"piece1").play("pieceAnim");
+            jeu.scene.physics.add.overlap(jeu.player.playerCenter,piece,this.collectPiece);
+        }
+    },
+    collectPiece : function(player,piece) {
+        jeu.scene.sound.play("collect");
+        piece.destroy();
+    },
+    creerAnimationPiece : function() {
+        jeu.scene.anims.create({
+            key : "pieceAnim",
+            frames : [
+              {key : "piece1"},
+              {key : "piece2",}
+            ],
+            frameRate : 8,
+            repeat : -1
+        });
     }
 }
